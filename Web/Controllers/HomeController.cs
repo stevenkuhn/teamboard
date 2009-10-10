64eac1a50a38d32ac4f383bc998c88c51dd353da
Steven Kuhn <email@stevenkuhn.net>
2009-10-10 13:57:41 -0500

    Upgraded ninject to 2.0.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Web.Controllers
+namespace TeamBoard.Web.Controllers
 {
 	[HandleError]
 	public class HomeController : Controller
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -4,29 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Ninject.Web.Mvc;
+using Ninject;
 
-namespace Web
+namespace TeamBoard.Web
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class MvcApplication : NinjectHttpApplication
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default",                                              // Route name
-                "{controller}/{action}/{id}",                           // URL with parameters
-                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-            );
+			routes.MapRoute(
+				 "Default",                                              // Route name
+				 "{controller}/{action}/{id}",                           // URL with parameters
+				 new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+			);
 
-        }
+		}
 
-        protected void Application_Start()
-        {
-            RegisterRoutes(RouteTable.Routes);
-        }
-    }
+		protected override void OnApplicationStarted()
+		{
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		protected override IKernel CreateKernel()
+		{
+			return new StandardKernel();
+		}
+	}
 }
